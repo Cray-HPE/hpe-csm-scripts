136e85b76d97b3a1a7a871bb1a3ed92146f94bb7
Matt Kelly <mpkelly@cray.com>
2021-05-18 09:16:30 -0500

    Added management switch port data
    When mismatches are found between SLS and HSM data, the BMCs in
    question are checked for association with management switch ports.
    If none are found, the BMC(s) in question are annotated as not having
    any port association.  This helps to determine if the mismatch is
    benign.

--- scripts/hms_verification/hsm_discovery_verify.sh
+++ scripts/hms_verification/hsm_discovery_verify.sh
@@ -5,18 +5,33 @@ echo "             HSM Discovery Verification                   "
 echo "=========================================================="
 
 SLSJSON="/tmp/sls.json"
+SLSRAW="/tmp/slsraw.json"
 COMPSJSON="/tmp/comps.json"
 RFEPSJSON="/tmp/rfeps.json"
 
+# Wrapper for grep, since if grep finds no match it returns 1 and breaks stuff.
+grepme () {
+	tok=$1
+	shift
+	grep $tok $*
+	return 0
+}
 
 echo " "
 echo "Fetching SLS Components..."
+
+cray sls hardware list --format json > ${SLSRAW}
+if [ `cat ${SLSRAW} | wc -l` -eq 0 ]; then
+	echo " "
+	echo "==> ERROR: Failed to get SLS Components."
+	exit 1
+fi
+
 cray sls hardware list --format json | jq '.[] | select ((.TypeString == "Node") or (.TypeString == "ChassisBMC") or (.TypeString == "RouterBMC") or (.TypeString == "CabinetPDUController")) | if .TypeString == "Node" then .Parent else .Xname end' | sort | uniq | sed 's/"//g' | sed 's/\(x.*c[0-9]$\)/\1b0/' > ${SLSJSON}
 
 if [ `cat ${SLSJSON} | wc -l` -eq 0 ]; then
 	echo " "
 	echo "==> ERROR: Failed to get SLS Components."
-	cat ${RSPOUT}
 	exit 1
 fi
 
@@ -27,7 +42,6 @@ cray hsm state components list --format json | jq '.Components[] | select((.Type
 if [ `cat ${COMPSJSON} | wc -l` -eq 0 ]; then
 	echo " "
 	echo "==> ERROR: Failed to get HSM Components."
-	cat ${RSPOUT}
 	exit 1
 fi
 
@@ -38,7 +52,6 @@ cray hsm inventory redfishEndpoints list --format json | jq '.RedfishEndpoints[]
 if [ `cat ${RFEPSJSON} | wc -l` -eq 0 ]; then
 	echo " "
 	echo "==> ERROR: Failed to get HSM Redfish endpoints."
-	cat ${RSPOUT}
 	exit 1
 fi
 
@@ -50,8 +63,19 @@ wcl=`comm -23 ${SLSJSON} ${COMPSJSON} | wc -l`
 if [ $wcl -eq 0 ]; then
 	echo "ALL OK"
 else
-	comm -23 ${SLSJSON} ${COMPSJSON}
 	rslt=1
+	IFS=$'\r\n' GLOBIGNORE='*' command eval 'BCOMPS=($(comm -23 ${SLSJSON} ${COMPSJSON}))'
+	nbcmps=${#BCOMPS[@]}
+
+	for (( ix = 0; ix < nbcmps; ix ++ )); do
+		hasport=$(cat $SLSRAW | jq '.[] | .ExtraProperties | .NodeNics' | sed 's/"//g' | sed 's/ //g' | grepme ${BCOMPS[$ix]})
+
+		if [ "${hasport}" == "" ]; then
+			echo "${BCOMPS[$ix]}  # No mgmt port association"
+		else
+			echo "${BCOMPS[$ix]}"
+		fi
+	done
 fi
 
 echo " "
@@ -60,8 +84,17 @@ wcl=`comm -23 ${SLSJSON} ${RFEPSJSON} | wc -l`
 if [ $wcl -eq 0 ]; then
 	echo "ALL OK"
 else
-	comm -23 ${SLSJSON} ${RFEPSJSON}
 	rslt=1
+	IFS=$'\r\n' GLOBIGNORE='*' command eval 'BRFEPS=($(comm -23 ${SLSJSON} ${RFEPSJSON}))'
+	nbrfeps=${#BRFEPS[@]}
+	for (( ix = 0; ix < nbrfeps; ix ++ )); do
+		hasport=$(cat $SLSRAW | jq '.[] | .ExtraProperties | .NodeNics' | sed 's/"//g' | sed 's/ //g' | grepme ${BRFEPS[$ix]})
+		if [ "${hasport}" == "" ]; then
+			echo "${BRFEPS[$ix]}  # No mgmt port association"
+		else
+			echo "${BRFEPS[$ix]}"
+		fi
+	done
 fi
 
 exit $rslt
