359d0ed88d48f9613a1e1dbdb216c8db1fc0d52f
Matt Kelly <matthew.kelly2@hpe.com>
2021-09-14 10:15:30 -0500

    Added script to automate the running of HMS CT tests.
    Added script to automate the running of HMS CT tests.
    
    DOES THIS CHANGE INVOLVE ANY SCHEME CHANGES? N
    
    REMINDER: HAVE YOU INCREMENTED VERSION NUMBERS? E.G., .spec, Chart.yaml N/A
    
    REMINDER 2: HAVE YOU UPDATED THE COPYRIGHT PER hpe GUIDELINES: © Copyright 2014-2020 Hewlett Packard Enterprise Development LP ? Y
    
    * Resolves CASMHMS-5058
    
    Tested on:
    
    * Wasp
    
    Was a fresh Install tested? N No chart or docker images involved.
    Was an Upgrade tested? N " "
    Was a Downgrade tested? N " "
    Was a CT test run? Y If not, Why?
    If schema changes were part of this change, how were those handled in your upgrade/downgrade testing? N/A
    
    Ran script on Wasp, verified that it ran all HMS CT tests correctly.
    
    Low risk, it's just an automation script.

---
+++ scripts/hms_verification/run_hms_ct_tests.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# MIT License
+# 
+# (C) Copyright [2021] Hewlett Packard Enterprise Development LP
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+
+ctSmokeLog="/tmp/ct_smoke_log.txt"
+ctFuncLog="/tmp/ct_func_log.txt"
+
+echo "================================================================="
+echo "============  Running HMS CT Smoke Tests... ====================="
+echo "================================================================="
+echo " "
+
+if [ ! -e /opt/cray/tests/ncn-resources/hms/hms-test/hms_run_ct_smoke_tests_ncn-resources.sh ]; then
+	echo " "
+	echo "===> CT Smoke test not found -- not installed?"
+	exit 1
+fi
+
+/opt/cray/tests/ncn-resources/hms/hms-test/hms_run_ct_smoke_tests_ncn-resources.sh > $ctSmokeLog 2>&1
+rval=$?
+
+if [[ $rval != 0 ]]; then
+	echo "CT Smoke Test Failed.  See output in ${ctSmokeLog}."
+	exit 1
+fi
+
+
+echo " "
+echo "================================================================="
+echo "===========  Running HMS CT Functional Tests... ================="
+echo "================================================================="
+echo " "
+
+if [ ! -e /opt/cray/tests/ncn-resources/hms/hms-test/hms_run_ct_functional_tests_ncn-resources.sh ]; then
+	echo " "
+	echo "===> CT Functional Test not found -- not installed?"
+	exit 1
+fi
+
+/opt/cray/tests/ncn-resources/hms/hms-test/hms_run_ct_functional_tests_ncn-resources.sh > $ctFuncLog 2>&1
+rval=$?
+
+if [[ $rval != 0 ]]; then
+	echo " "
+	echo "===> CT Functional Test Failed.  See output in ${ctFuncLog}."
+	exit 1
+fi
+
+exit 0
+
